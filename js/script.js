e6f6badd1b7950c664407355d9484d5f85d0e5aa
Malgorzata Berdys <berdysm@gmail.com>
2020-05-30 19:18:55 +0200

    Add active class to clicked link

--- js/script.js
+++ js/script.js
@@ -5,16 +5,19 @@ function titleClickHandler(event){
   console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
   /* add class 'active' to the clicked link */
+const clickedElement = this;
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active')
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+const activeArticles = document.querySelectorAll('.posts article.active');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
