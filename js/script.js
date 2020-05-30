79fa5cc185148eef320dd83782881a1bde5efe58
Malgorzata Berdys <berdysm@gmail.com>
2020-05-30 20:15:24 +0200

    Add articleSelector and targetArticle const

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+event.preventDefault();
 const clickedElement = this;
 console.log('clickedElement:', clickedElement);
-clickedElement.classList.add('active')
+clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts article.active');
@@ -23,11 +24,17 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
