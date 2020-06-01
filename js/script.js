c64484dee05e66c192c2e083e69e50bdd3e20982
Malgorzata Berdys <berdysm@gmail.com>
2020-06-01 21:45:10 +0200

    Finish article loop

--- js/script.js
+++ js/script.js
@@ -52,23 +52,25 @@ function generateTitleLinks(){
   console.log('Link was generated!');
 
   /* remove contents of titleList */
-  const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
   console.log('list is clear');
 
   /* for each article */
   const articles = document.querySelectorAll(optArticleSelector);
 
   for(let article of articles){
-   
-  }
+    console.log(article);
   /* get the article id */
-
+  const articleId = article.getAttribute('id');
   /* find the title element */
-
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
   /* get the title from the title element */
-
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(articleId + articleTitle);
   /* create HTML of the link */
-
+  titleList.insertAdjacentHTML('afterbegin', linkHTML);
+  }
   /* insert link into titleList */
 
 }
